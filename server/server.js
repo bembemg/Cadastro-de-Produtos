16a7f87710cd3fe6002ef721b959f791d3c64e79
Gabriel Bembem <92057057+bembemg@users.noreply.github.com>
2025-01-10 22:56:36 -0300

    deploy

--- server/server.js
+++ server/server.js
@@ -9,13 +9,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Local
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_PORT,
+//     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+// });
+
+// Deploy
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 async function initializeDB() {
